07fffb025e3fd833a9f36e215449a4ce99b06782
Christopher Eng <50420314+engchristopher@users.noreply.github.com>
2020-01-06 09:44:56 -0800

    Update Web Scraping.r
    Example script to web scrape data using R

--- R/Web Scraping.r
+++ R/Web Scraping.r
@@ -1,13 +1,13 @@
 #How to web scrape information off of a webpage in Rstudio
 library(‘rvest’)
-url <- ’https://www.franchisegrade.com/search?page=1&results=2484'
+url <- 'WEBSITE URL TO SCRAPE'
 webpage <- read_html(url)
 title_data_html <- html_nodes(webpage,‘p.h4.search-name.text-center’)
 title_data <- html_text(title_data_html)
 head(title_data)
 
 library(‘rvest’)
-url <- ‘WEBSITE URL TO SCRAPE’
+url <- 'WEBSITE URL TO SCRAPE'
 webpage <- read_html(url)
 title_data_html <- html_nodes(webpage,‘p.text-center’)
 title_data <- html_text(title_data_html)
