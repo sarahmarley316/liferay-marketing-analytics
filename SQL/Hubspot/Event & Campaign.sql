ed65d8fc558dbec901834a97e53b0e4bdf99d7b9
Christopher Eng <50420314+engchristopher@users.noreply.github.com>
2020-01-06 09:13:41 -0800

    Create Event & Campaign.sql
    Grabs all recipients who have interacted with an email and returns their email metrics.  If you just want email metrics not tied to a person, use only the Campaign object and properties.

---
+++ SQL/Hubspot/Event & Campaign.sql
@@ -0,0 +1,20 @@
+#Query for Emails
+SELECT if(Event.created<>'',from_unixtime(floor(Event.created/1000)),null) as createddate_email
+		,Event.recipient
+		,Event.type
+		,CampaignMember_.name
+		,CampaignMember.subject
+		,CampaignMember.counterBounced as Bounced
+		,CampaignMember.counterclicked as Clicked
+		,CampaignMember.counterDeferred as Deferred
+		,CampaignMember.counterDropped as Dropped 
+		,CampaignMember.counterForward as Forward
+		,CampaignMember.counterProcessed as Processed
+		,CampaignMember.counterOpened as Opened
+		,CampaignMember.counterReplied as Replied
+		,CampaignMember.counterSent as Sent
+		,CampaignMember.counterUnsubscribed as Unsubscribed
+		,if(CampaignMember.lastUpdatedTime<>'',from_unixtime(floor(CampaignMember.lastUpdatedTime/1000)),null) as lastUpdatedTime
+FROM Event
+INNER JOIN Campaign
+	ON Event.emailCampaignId = CampaignMember.id
